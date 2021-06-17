539d7c0db10d7ed26a40a6016432bd8d3f16be90
Leandro <syiabonga111@gmail.com>
2021-06-17 04:41:36 -0500

    nav menu and background image added

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,7 +1,36 @@
 <template>
     <div class="conatiner-fluid">
         <div class="main-banner">
-            <h1>Landing</h1>
+            <div class="menu">
+                <nav class="navbar navbar-expand-lg">
+                    <div class="container-fluid">
+                        <strong>
+                            <a class="navbar-brand" href="#">BarberShop</a>
+                        </strong>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarText">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Barbers</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">All Appointments</a>
+                            </li>
+                        </ul>
+                        <span class="navbar-text">
+                            Login
+                        </span>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="text-center">
+                <h1>More than just a haircut</h1>
+                <h1>A life style</h1>
+                <button type="button" class="btn btn-primary">Book an appoitment</button>
+            </div>
         </div>
     </div>
 </template>
@@ -19,5 +48,33 @@ export default {
         background-size: contain;
         background-repeat: no-repeat;
         background-position: center;
+    }
+    nav a{
+        color: white !important;
+    }
+    nav ul{
+        margin-left: 100px;
+    }
+    nav ul li{
+        margin: 5px;
+    }
+    nav ul li a:hover{
+        color: #4D78CA !important;
+    }
+    .text-center{
+        text-align: left !important;
+        padding: 10%;
+        font-weight: bolder;
+    }
+    .text-center button{
+        background-color: #6096FD !important;
+        border: #6096FD;
+        border-radius: 0;
+        width: 200px;
+        height: 50px;
+    }
+    .text-center button:hover{
+        background-color: #4D78CA !important;
+        transition: 0.5s;
     }
 </style>
